f8559efeb60b8b2e74f17d6223534428f3440442
WBW28 <maximewang666@gmail.com>
2023-10-26 23:54:48 +0200

    dev

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+/* eslint-disable no-console, no-alert */
+<template>
+    <div>
+        <span>©2023 · 晓文同学持续输出中... Copyright©图书网 ALL Rights Reserved</span>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped lang="scss"></style>
